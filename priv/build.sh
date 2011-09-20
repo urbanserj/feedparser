5a26bef366c544c8e1382015ab8fceb959e49137
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2011-09-20 23:10:44 +0400

    add support virtualenv-2.[756] in build.sh

--- priv/build.sh
+++ priv/build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+
+for filename in /usr/bin/virtualenv-2.7 /usr/bin/virtualenv-2.6 /usr/bin/virtualenv-2.5; do
+	if [ -f $filename ]; then
+		VIRTUALENV=$filename
+		break
+	fi
+done
+if [ -z $VIRTUALENV ]; then
+	VIRTUALENV=/usr/bin/virtualenv
+fi
+
 ENVDIR="`dirname $0`/env"
 [ -d $ENVDIR ] && exit 0
-virtualenv $ENVDIR
+$VIRTUALENV $ENVDIR
 $ENVDIR/bin/easy_install erlport
 $ENVDIR/bin/easy_install feedparser
 $ENVDIR/bin/easy_install chardet
