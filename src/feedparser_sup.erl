22c20df5ed506eaaa12cc9e8554dc3e363f088ad
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2011-10-14 15:59:11 +0400

    add poll_size to default app's config

--- src/feedparser_sup.erl
+++ src/feedparser_sup.erl
@@ -14,18 +14,15 @@
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-	PoolSize = case application:get_env(feedparser, pool_size) of
-		{ok, L} -> L;
-		_ -> 4
-	end,
-    {ok, {{one_for_one, 10, 10}, [{feedparser,
+	{ok, PoolSize} = application:get_env(feedparser, pool_size),
+	{ok, {{one_for_one, 10, 10}, [{feedparser,
 		{poolboy, start_link, [[
 			{name, {local, feedparser}},
 			{worker_module, feedparser},
