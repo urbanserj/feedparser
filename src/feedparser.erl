8c6d168186400eca88727a31d9b17e442f51c49f
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2012-02-04 18:08:30 +0400

    move term_to_binary & binary_to_term to client

--- src/feedparser.erl
+++ src/feedparser.erl
@@ -24,7 +24,9 @@ parse(Data) ->
 parse(Data, Headers) ->
 	Worker = poolboy:checkout(feedparser, true, infinity),
 	try
-		gen_server:call(Worker, {parse, Data, Headers}, ?TIMEOUT)
+		BinData = term_to_binary({parse, Data, Headers}),
+		RBinData = gen_server:call(Worker, {parse, BinData}, ?TIMEOUT),
+		binary_to_term(RBinData)
 	after
 		poolboy:checkin(feedparser, Worker)
 	end.
@@ -35,28 +37,32 @@ parse(Data, Headers) ->
 
 init(_) ->
 	Dir = case code:priv_dir(?MODULE) of
-		{error, _} -> filename:join( [ filename:dirname(code:which(?MODULE)), "..", "priv" ] );
-		PrivDir -> PrivDir
+		{error, _} ->
+			ModuleDir = filename:dirname(code:which(?MODULE)),
+			filename:join( [ModuleDir, "..", "priv" ]);
+		PrivDir ->
+			PrivDir
 	end,
 	AbsDir = filename:absname(Dir),
 	Python = filename:join(AbsDir, "./python"),
 	Port = open_port({spawn_executable, Python},
-		[{packet, 4}, binary, use_stdio, exit_status, {cd, AbsDir}, {args, ["./feedparser-port.py"]}]
+		[{packet, 4}, binary, use_stdio, exit_status,
+		{cd, AbsDir}, {args, ["./feedparser-port.py"]}]
 	),
-	{ok, #state{port = Port}}.
+	{ok, #state{port=Port}}.
 
-handle_call({parse, Data, Headers}, From, #state{port = Port, queue = Queue} = State) ->
-	port_command(Port, term_to_binary({parse, Data, Headers})),
-	{noreply, State#state{queue = queue:in(From, Queue)}, ?TIMEOUT}.
+handle_call({parse, Data}, From, #state{port=Port, queue=Queue}=State) ->
+	port_command(Port, Data),
+	{noreply, State#state{queue=queue:in(From, Queue)}, ?TIMEOUT}.
 
 handle_cast(_Request, State) ->
 	{noreply, State}.
 
-handle_info({Port, {data, Data}}, #state{port = Port, queue = FQueue} = State) ->
+handle_info({Port, {data, Data}}, #state{port=Port, queue=FQueue}=State) ->
 	{{value, From}, Queue} = queue:out(FQueue),
-	gen_server:reply(From, binary_to_term(Data)),
-	{noreply, State#state{queue = Queue}};
-handle_info({Port, {exit_status, _Code}}, #state{port = Port} = State) ->
+	gen_server:reply(From, Data),
+	{noreply, State#state{queue=Queue}};
+handle_info({Port, {exit_status, _Code}}, #state{port=Port}=State) ->
 	{stop, port_exit, State};
 handle_info(timeout, State) ->
 	{stop, timeout, State};
