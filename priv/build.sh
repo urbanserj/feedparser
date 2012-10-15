36484294d1fc1bc8baa60e1c4dbaedfb33ac4ff8
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2012-10-15 16:04:54 +0400

    set $VIRTUALENV after check

--- priv/build.sh
+++ priv/build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -e
 
-for version in 2.7 2.6 2.5; do
-	VIRTUALENV=`which virtualenv-$version 2> /dev/null || echo`
-	[ -n ""$VIRTUALENV ] && break
-done
-[ -z $VIRTUALENV ] && VIRTUALENV=`which virtualenv` || \
-	(echo virtualenv: command not found >&2 && exit 1)
-VER=`echo $VIRTUALENV | sed -re 's/.*virtualenv-?(.[.].)?/\1/'`
-
 cd `dirname $0`
 [ -d chardet -a \
   -d erlport -a \
   -f feedparser.py -a \
   ! -d env ] && exit 0
 rm -rf env chardet erlport feedparser.py *.pyc
+
+for version in 2.7 2.6 2.5; do
+	VIRTUALENV=`which virtualenv-$version 2> /dev/null || echo`
+	[ -n ""$VIRTUALENV ] && break
+done
+[ -z $VIRTUALENV ] && VIRTUALENV=`which virtualenv 2> /dev/null` || \
+	(echo virtualenv: command not found >&2 && exit 1)
+VER=`echo $VIRTUALENV | sed -re 's/.*virtualenv-?(.[.].)?/\1/'`
 
 $VIRTUALENV env
 
